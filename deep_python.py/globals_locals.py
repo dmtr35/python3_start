07ad2e5a560afd45cc7a1550af55dd0392cf15ce
dmtr35 <nuke-id@yandex.ru>
2025-10-02 19:42:21 +0300

    Auto-commit: 02-10-2025 19:42:21 Added:    jail/01_jail.py Modified: deep_python.py/globals_locals.py

--- deep_python.py/globals_locals.py
+++ deep_python.py/globals_locals.py
@@ -2,11 +2,16 @@ import globals_2
 
 y = 20
 
-def show():
-    print("b.globals:", globals().keys())
-    print("b.globals:", globals()['globals_2'].x)
+def show(x):
+    a = 66
+    # print("b.globals:", globals().keys())
+    # print("b.globals:", globals()['globals_2'].x)
+    print("loc:", locals().keys())
+    print("dir:", dir(a))
+
+print("loc_here:", locals().keys())
+print("dir_here:", dir())
 
 globals_2.show()
-show()
-
+show(33)
 
---
+++ jail/01_jail.py
@@ -0,0 +1,13 @@
+import string
+
+code = input('Your scientific computation: ')
+
+code = ''.join([c for c in code if c in string.printable])
+
+forbidden = ['eval', 'exec', 'import', 'open', 'system', 'os', 'builtins']
+for f in forbidden:
+    code = code.replace(f, '')
+    print(code)
+
+print(code)
+exec(code)
