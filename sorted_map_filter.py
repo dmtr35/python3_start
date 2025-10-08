50dd56cf2b04b7b10c16e486b9089094baea6f42
dmtr35 <nuke-id@yandex.ru>
2025-10-08 17:10:06 +0300

    Auto-commit: 08-10-2025 17:10:06 Modified: sorted_map_filter.py

--- sorted_map_filter.py
+++ sorted_map_filter.py
@@ -9,6 +9,9 @@ points = [ { 'x' : 2, 'y' : 3 }, { 'x' : 4, 'y' : 1 } ]
 print(sorted(points, key=lambda x: x['y']))
 
 
+# sorted возвращает новый результат, fruits.sort() редактирует fruits
+
+
 # ====================================================================
 # map 
 nums = [1, 2, 3, 4]
@@ -19,3 +22,22 @@ print(list(map(lambda x, y: x+y, nums, nums2)))
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list(filter(lambda x: x%2, nums)))
 print(list(filter(lambda x: x%2 == 0, nums)))
+
+# ====================================================================
+
+# сортировка object
+
+fruits = {'apple': 4, 'banana': 3, 'kiwi': 2, 'pomegranate': 1}
+
+print(dict(sorted(fruits.items(), key=lambda x: x[1])))                # сортировать dict по value
+print(dict(sorted(fruits.items(), key=lambda x: x[1], reverse=True)))  # по value в обратнов порядке
+
+
+print(dict(sorted(fruits.items(), key=lambda x: x[0])))                # сортировать по ключу
+# ====================================================================
+
+
+
+
+
+
