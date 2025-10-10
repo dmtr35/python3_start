156ce8340446230128f8b6c23cd42229f286ee41
dmtr35 <nuke-id@yandex.ru>
2025-10-10 19:18:14 +0300

    Auto-commit: 10-10-2025 19:18:14 Modified: test-ignore.py

--- test-ignore.py
+++ test-ignore.py
@@ -1,14 +1,9 @@
+# class F(Exception):
+#     __add__ = exec
 
-
-
-
-
-class F(Exception):
-    __add__ = exec
-
-f=F()
-f.__add__("__import__('os').system('ls')")
-f + "__import__('os').system('ls')"
+# f=F()
+# f.__add__("__import__('os').system('ls')")
+# f + "__import__('os').system('ls')"
 
 
 # print("F:", type(F))
@@ -19,3 +14,18 @@ f + "__import__('os').system('ls')"
 # except F as e:
 #     print("e:", type(e))
 #     e + "__import__('os').system('ls')"
+
+
+
+def sec(num):
+    for i in range(num):
+        return i+1
+    
+def fun(num):
+    print("first")
+    arr = [100, 99, 98]
+    for i in range(num):
+        yield from ["a", "b", "c"]
+    print("second")
+
+print(list(fun(5)))
