5fbf1669655acac0ed36c20158a85a882f8bcaab
dmtr35 <nuke-id@yandex.ru>
2025-11-19 20:39:17 +0200

    Auto-commit: 19-11-2025 20:39:17 Modified: .gitignore, import_examples/04_pathlib.py, scapy/mail_sniffer.py

--- scapy/mail_sniffer.py
+++ scapy/mail_sniffer.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
-from scapy.all import sniff
+from scapy.all import sniff, TCP, IP
 
 def packet_callback(packet):
-    print(packet.show())
+    print('here')
+
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f"[*] Destination: {packet[IP].dst}")
+            print(f"[*] {str(packet[TCP].payload)}")
+
 
 def main():
-    sniff(prn=packet_callback, count=1)
+    # store=0 - не будет хранить пакеты в памяти
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 143',
+           prn=packet_callback, store=0)
 
 if __name__ in "__main__":
     main()
